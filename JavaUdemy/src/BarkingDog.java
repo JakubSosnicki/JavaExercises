f4eb93822abac610fecedf14f0776cd2bb00dae7
JakubSosnicki <kubasosnicki@gmail.com>
2024-07-18 20:39:41 +0200

    Exercise 4 Barking Dog done - Method in Java practising

---
+++ JavaUdemy/src/BarkingDog.java
@@ -0,0 +1,18 @@
+public class BarkingDog {
+    public static void main(String[] args) {
+        System.out.println(shouldWakeUp (true, 1));
+        System.out.println(shouldWakeUp (false, 2));
+        System.out.println(shouldWakeUp (true, 8));
+        System.out.println(shouldWakeUp (true, -1));
+    }
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay){
+        if(hourOfDay<0 || hourOfDay>23)
+            return false; //incorrect hour
+
+        if(barking && (hourOfDay<8 || hourOfDay>22))
+            return true; //wake up
+        else
+            return false;
+
+    }
+}
