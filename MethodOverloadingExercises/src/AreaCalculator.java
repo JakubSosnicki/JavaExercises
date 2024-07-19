048c90b1984fb519bb5b7834385332cefd7dcae8
JakubSosnicki <kubasosnicki@gmail.com>
2024-07-19 16:12:01 +0200

    Exercise 9 AreaCalculator done - Method Overload Practising in Java

--- MethodOverloadingExercises/src/AreaCalculator.java
+++ MethodOverloadingExercises/src/AreaCalculator.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class AreaCalculator {
+import static java.lang.Math.pow;
+import static java.lang.Math.PI;
+
+public class AreaCalculator {
+    public static void main(String[] args) {
+        System.out.println(area(5.0));
+        System.out.println(area(-1));
+        System.out.println(area(5.0, 4.0));
+        System.out.println(area(-1.0, 4.0));
+    }
+
+    //circle area
+    public static double area(double radius){
+        if(radius<0)
+            return -1;
+        return pow(radius,2)*PI;
+    }
+
+    //rectangle area
+    public static double area(double x, double y){
+        if(x<0 || y<0)
+            return -1;
+        return x*y;
+    }
 }
